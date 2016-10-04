46ffab7b3adea41cdb6591c9254600d690835823
Matthew Crowson <matthew.d.crowson@gmail.com>
2016-10-04 12:04:38 -0400

    manifest automatically pulls in packages. Change to format of lambda packages. Used to be dict of dict of dict. Now id dict of list of dict to support multiple versions

--- lambda_packages/__init__.py
+++ lambda_packages/__init__.py
@@ -1,54 +1,16 @@
 import os
 
 # A manifest of the included packages.
-lambda_packages = {
-    'cffi': {
-        'version': '1.7.0',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'cffi', 'cffi-1.7.0.tar.gz')
-    },
-    'cv2': {
-        'version': '3.1.0',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'OpenCV', 'OpenCV-3.1.0.tar.gz')
-    },
-    'lxml': {
-        'version': '3.6.0',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lxml', 'lxml-3.6.0.tar.gz')
-    },
-    'MySQL-Python': {
-        'version': '1.2.5',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'MySQL-Python', 'MySQL-Python-1.2.5.tar.gz')
-    },
-    'numpy': {
-        'version': '1.10.4',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'numpy', 'numpy-1.10.4.tar.gz')
-    },
-    'Pillow':{
-    	'version':'3.1.1',
-    	'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Pillow', 'Pillow-3.1.1.tar.gz')
-    },
-    'psycopg2': {
-        'version': '2.6.1',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'psycopg2', 'psycopg2-2.6.1.tar.gz')
-    },
-    'pycrypto': {
-        'version': '2.6.1',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'pycrypto', 'pycrypto-2.6.1.tar.gz')
-    },
-    'pynacl': {
-        'version': '1.0.1',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'PyNaCl', 'PyNaCl-1.0.1.tar.gz')
-    },
-    'cryptography': {
-        'version': '1.4',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'cryptography', 'cryptography-1.4.tar.gz')
-    },
-    'pyproj': {
-        'version': '1.9.5',
-        'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 
-                             'pyproj', 'pyproj.4-4.9.2.tar.gz')
-    }
-}
+cwd = os.path.abspath(__file__)
+pkg_extension = '.tar.gz'
+lambda_packages = dict()
+folders = [f for f in os.listdir(cwd) if os.path.isdir(os.path.join(cwd, f))]
+for folder in folders:
+    this_package = list()
+    gz_tarballs = [f for f in os.listdir(os.path.join(cwd, folder)) if pkg_extension in f]
+    for file_name in gz_tarballs:
+        this_version = dict()
+        this_version['path'] = os.path.join(os.path.dirname(cwd), folder, file_name)
+        this_version['version'] = file_name.strip(pkg_extension).split('-', 1)[1]
+        this_package.append(this_version)
+    lambda_packages[folder.lower()] = this_package
